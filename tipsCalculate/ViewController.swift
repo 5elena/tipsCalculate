038709082de2d255ac020ade529ca836ba99c784
Selena <selena@hawaii-macbook-air.local>
2018-09-09 14:29:40 +0800

    Initial Commit

---
+++ tipsCalculate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipsCalculate
+//
+//  Created by Selena on 2018/9/9.
+//  Copyright © 2018年 Selena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
